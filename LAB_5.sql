29194448a0ec4c40c259f19f37ded5f0c01d81e0
mgorczynsky2 <115210575+mgorczynsky2@users.noreply.github.com>
2022-12-02 09:08:00 +0100

    Update LAB_5.sql

--- LAB_5.sql
+++ LAB_5.sql
@@ -11,28 +11,37 @@ CREATE table gorczynskim.ekwipunek AS SELECT * from wikingowie.ekwipunek;
 SELECT * from kreatura WHERE NOT rodzaj='wiedzma' AND udzwig > 50 or udzwig = 50;
 #2.2
 SELECT * from zasob WHERE waga >= 2 and waga <=5;
-#2.3
+SELECT * from kreatura WHERE waga between 2 and 5;
+#2.3s
 SELECT * from kreatura WHERE nazwa like '%or%' AND UDZWIG BETWEEN 30 and 100;
 #-----------------------------------------------------zad 3-------------------------------------------
 #3.1
 SELECT * from zasob  where month(dataPozyskania) = 7 OR month(dataPozyskania) = 8;
+							#month(dataPozyskania) in (7,8);
+#month(dataPozyskania)
+#year(dataPozyskania)
+#day(dataPozyskania)
 #3.2
 select * from zasob ORDER BY waga ASC;
 #3.3
-SELECT * FROM kreatura ORDER BY dataUr ASC limit 5;
+SELECT * FROM kreatura ORDER BY dataUr DESC limit 5;
+									#DESC limit 5,5; ---zaczynamy od 5 rekordu i wyswietlamy 5
 #-----------------------------------------------------zad 4-------------------------------------------
 #4.1
 SELECT DISTINCT rodzaj from zasob;
+	   #DISTINCT(rodzaj)
 #4.2
+SELECT CONCAT("nazywam sie ",nazwa, " i mój wiek to ", wiek) from postac;
 SELECT CONCAT(nazwa,' ',rodzaj) AS 'nazwa-rodzaj' FROM zasob;
 #4.3
 SELECT * from zasob;
 SELECT nazwa,ilosc*waga AS waga_calkowita FROM zasob WHERE dataPozyskania >= '2000-01-01' AND dataPozyskania <= '2007-12-30';
+SELECT nazwa,ilosc*waga AS waga_calkowita FROM zasob WHERE year(dataPozyskania) between 2000 and 2007;
 #-----------------------------------------------------zad 5--------------------------------------------
 #5.1
-SELECT nazwa,waga*0.7 AS 'masa wlasciwa jedzenia', waga*0.3 AS 'waga odpadkow' from zasob WHERE rodzaj='jedzenie';
+SELECT nazwa,(ilosc*waga)*0.7 AS 'masa wlasciwa jedzenia', (ilosc*waga)*0.3 AS 'waga odpadkow' from zasob WHERE rodzaj='jedzenie';
 #5.2
 SELECT * from zasob where rodzaj is null;
 #5.3
 SELECT * from zasob;
-SELECT DISTINCT nazwa,rodzaj from zasob WHERE nazwa like '%os' AND nazwa like 'Ba%' ORDER BY rodzaj ASC;
+SELECT DISTINCT (rodzaj) from zasob WHERE  nazwa like 'Ba%' OR nazwa like '%os' ORDER BY rodzaj ASC;
