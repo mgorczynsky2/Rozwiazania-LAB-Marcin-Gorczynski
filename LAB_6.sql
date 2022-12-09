a2e7e1435e7f696797e336ac30473886e73d59f1
mgorczynsky2 <115210575+mgorczynsky2@users.noreply.github.com>
2022-12-09 10:16:04 +0100

    Update LAB_6.sql

--- LAB_6.sql
+++ LAB_6.sql
@@ -19,24 +19,40 @@ SELECT sum(waga),nazwa,avg(waga) from zasob group by nazwa HAVING avg(waga)>=4 A
 SELECT rodzaj,count(distinct(nazwa)) from zasob WHERE ilosc >1 GROUP by rodzaj;
 #---------------------------------------------zad3-------------------------------------------------------
 #3.1
+select * from kreatura,ekwipunek where kreatura.idKreatury=ekwipunek.idKreatury;
+#inner join
+select * from kreatura k inner join ekwipunek e on k.idKreatury=e.idKreatury;
 select * from kreatura;
 select * from ekwipunek;
 select * from zasob;
-SELECT nazwa,ilosc from kreatura LEFT JOIN ekwipunek ON kreatura.idKreatury = ekwipunek.idKreatury GROUP by nazwa;
+SELECT nazwa,ilosc from kreatura INNER JOIN ekwipunek ON kreatura.idKreatury = ekwipunek.idKreatury GROUP by nazwa;
 #3.2
 SELECT kreatura.nazwa,zasob.nazwa FROM kreatura INNER JOIN ekwipunek on kreatura.idKreatury=ekwipunek.idKreatury 
 INNER JOIN zasob ON (ekwipunek.idZasobu=zasob.idZasobu);
 #3.3
 SELECT kreatura.nazwa,ekwipunek.idKreatury FROM kreatura LEFT JOIN ekwipunek on (kreatura.idKreatury=ekwipunek.idKreatury) 
 WHERE ekwipunek.idEkwipunku is null;
+#LEFT JOIN BIERZE WSZYSTKO Z ZBIORU A;
+#select * idKreatury from kreatura where id kreatury not in (select distinct idKreatury from ekwipunek WHERE idKreatury is not null); 
 #---------------------------------------------zad4-------------------------------------------------------
 #4.1
 SELECT kreatura.nazwa,kreatura.dataUr,zasob.nazwa from kreatura 
 INNER JOIN ekwipunek on kreatura.idKreatury=ekwipunek.idKreatury INNER JOIN zasob ON (ekwipunek.idZasobu=zasob.idZasobu)
 WHERE year(dataUr) between 1670 and 1680;
+SELECT kreatura.nazwa,kreatura.dataUr from kreatura natural join ekwipunek;
+
 #4.2
 SELECT kreatura.nazwa,kreatura.dataUr,zasob.nazwa from kreatura 
 INNER JOIN ekwipunek on kreatura.idKreatury=ekwipunek.idKreatury INNER JOIN zasob ON (ekwipunek.idZasobu=zasob.idZasobu)
-WHERE zasob.rodzaj ='jedzenie';
+WHERE zasob.rodzaj ='jedzenie' ORDER BY dataUr asc limit 5;
+select * from kreatura;
 #4.3
-SELECT * from kreatura where
+SELECT k1.nazwa,k2.nazwa FROM kreatura k1 INNER JOIN kreatura k2 on (k1.idKreatury+5=k2.idKreatury);
+#---------------------------------------------zad5-------------------------------------------------------
+#5.1
+SELECT kreatura.rodzaj,avg(zasob.waga*ekwipunek.ilosc) AS srednia from kreatura INNER JOIN ekwipunek ON (kreatura.idKreatury=ekwipunek.idKreatury)
+INNER JOIN zasob ON (ekwipunek.idZasobu=zasob.idZasobu) WHERE kreatura.rodzaj!='malpa' OR kreatura.rodzaj!='waz' 
+GROUP by rodzaj having sum(ekwipunek.ilosc)<30;
+#5.2
+SELECT kreatura.nazwa,kreatura.rodzaj,kreatura.dataUr FROM kreatura ,(SELECT min(dataUr) AS min,max(dataUr) AS max from kreatura group by rodzaj) e2 
+WHERE dataUr = min OR dataUr=max;
